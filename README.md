47bec1a4d56847b78f01e349d353e977d51e3ea3
Youssef Kaiboussi <ykaiboussi@enova.com>
2019-12-20 15:32:44 -0600

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# subnet_scanning
+
+Big credit to https://github.com/21y4d/nmapAutomator
+
+
+
+# Usage
+
+```
+go build main.go
+
+./main -subnet=IP-RANGE -numWorkers=5 -scanType=Basic
+```
+
+Virus Healtch Check:
+[status](https://www.virustotal.com/gui/url/fbf05ada06b3112d5c9ba0f673ea7265bfdf3d7e039a045f0d557851e4868174/detection)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+)
+
+var (
+	path       = "./"
+	subnet     *string
+	numWorkers *int
+	scanType   *string
+)
+
+func init() {
+	numWorkers = flag.Int("numWorkers", 3, "numberOfWorker")
+	subnet = flag.String("subnet", " ", "a string")
+	scanType = flag.String("scanType", "Basic", "a string")
+	flag.Parse()
+}
+
+// Here's the worker, of which we'll run several
+// concurrent instances. These workers will receive
+// work on the `jobs` channel and send the corresponding
+// results on `results`. We'll sleep a second per job to
+// simulate an expensive task.
+func worker(ip string, id int, jobs <-chan int, wg *sync.WaitGroup) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job", j)
+		command := `./bin/nmapautomator.sh ` + ip + ` ` + *scanType
+		_, err := exec.Command("/bin/sh", "-c", command).Output()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		fmt.Println("worker", id, "finished job", j)
+		wg.Done()
+	}
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	err := os.Chdir("./")
+	if err != nil {
+		log.Fatal("failed change to root project directory error: ", err)
+	}
+
+	ips := ipList(subnet)
+
+	// In order to use our pool of workers we need to send
+	// them work and collect their results. We make 2
+	// channels for this.
+	jobs := make(chan int, len(ips))
+
+	// This starts up 3 workers, initially blocked
+	// because there are no jobs yet.
+	for _, ip := range ips {
+		wg.Add(1)
+		go worker(ip, len(ips), jobs, &wg)
+
+	}
+
+	// Here we send 5 `jobs` and then `close` that
+	// channel to indicate that's all the work we have.
+	for j := 1; j <= *numWorkers; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Finally we collect all the results of the work.
+	// This also ensures that the worker goroutines have
+	// finished.
+	wg.Wait()
+}
+
+func ipList(subnet *string) []string {
+	listIPs := []string{}
+	command := `nmap -sL -n ` + *subnet + ` | grep 'Nmap scan report for' | cut -f 5 -d ' ' > targets.txt`
+	_, err := exec.Command("/bin/sh", "-c", command).Output()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	file, _ := os.Open(path + "targets.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		listIPs = append(listIPs, scanner.Text())
+	}
+
+	return listIPs
+}
